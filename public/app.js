12efb32580ca1671411b46d861779f8ed0fe1f21
cecigiunta <ceciliagiunta57@gmail.com>
2022-08-31 22:51:00 -0300

    cambios en header, home y filtros x busqueda

--- public/app.js
+++ public/app.js
@@ -2,11 +2,14 @@ Vue.createApp({
     data() {
         return {
             url: './api.json',
+            search: '',
             results: [],
-            filtered: [], 
+            filtered: [],
+            detalle: [],
+            gameId : '' 
     }
     },
-    mounted(){
+    created(){
         fetch(this.url)
             .then((res) => res.json())
             .then((data) => {
@@ -16,7 +19,22 @@ Vue.createApp({
             .catch(err => console.error(err))
     },
     methods: {
-
-
+        filtrarId: function(game){
+            console.log(game._id);
+            this.gameId = game._id
+            this.filtered = this.results.filter(game => game._id === this.gameId)
+            console.log(this.filtered);
+            return this.filtered
+        },    
+    },
+    computed: {
+        filtrar: function(){
+            this.filtered = this.results.filter(game => 
+                game.team1.toLowerCase().includes(this.search.toLowerCase()) ||
+                game.team2.toLowerCase().includes(this.search.toLowerCase()) ||
+                game.location.toLowerCase().includes(this.search.toLowerCase())
+                )
+        },
+ 
         },            
 }).mount('#app')
