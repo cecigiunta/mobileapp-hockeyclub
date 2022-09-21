48675cfb264b6505624690824da00b19ce27af6f
cecigiunta <ceciliagiunta57@gmail.com>
2022-09-21 20:01:36 -0300

    registro, login y logout en firebase

--- public/app.js
+++ public/app.js
@@ -5,11 +5,44 @@ Vue.createApp({
             search: '',
             gameId : '',
             page: 'Home', 
+
+            
+            //registro variables
+            user_register: '',
+            email_register: '',
+            password_register: '',
+            
+            //login variables
+            user_login: '',
+            password_login: '',
+            isLogged: false,
+            whiteborder : document.getElementsByClassName('white-border'),
+            loginError: document.getElementsByClassName('login-error'),
+            
+            //usuario
+            alias: '', //el nombre q voy a mostrar en la app
+            usuario: null,
+
             results: [],
             filtered: [],
             detalle: [],
             september_games: [],
             october_games: [],
+            posts: [{
+                id: 1,
+                user: 'Juan',
+                text: 'This is the first post',
+                created_at: new Date(),
+                updated_at: new Date(),
+                }, 
+                {
+                id: 2,
+                user: 'Jane',
+                text: 'This is the second post',
+                created_at: new Date(),
+                updated_at: new Date(),
+                },
+            ],
     }
     },
     created(){
@@ -38,18 +71,71 @@ Vue.createApp({
             })
             return newArr   
         },
+        //* Ya está OK :)
+        register() {
+            if (this.email_register != '' && this.password_register != '') {
+                firebase.auth().createUserWithEmailAndPassword(this.email_register, this.password_register)
+                .then((userCredential) => {
+                    var user = userCredential.user;
+                    console.log(user)
+                    // lo que va a pasar, cuando termine de registrarse
+                    this.page = 'Login'
+                        this.email_register = ''
+                        this.password_register = ''
+                    })
+                    .catch((error) => {
+                        const errorCode = error.code;
+                        const errorMessage = error.message;
+                        console.log(errorCode)
+                        console.log(errorMessage)
+                        //MANEJAR ERROR CON ALEERTS, SWEET ALERT O ALGO MAS
+                    });
+                }
+            },
+            logIn() {
+                if (this.user_login != '' && this.password_login != '') {
+                    firebase.auth().signInWithEmailAndPassword(this.user_login, this.password_login)
+                    .then((userCredential) => {
+                        var user = userCredential.user;
+                        
+                        this.usuario = user
+                        this.alias = this.usuario.email
+                        this.isLogged = true
+                        this.page = 'Home'
+                        this.user_login = ''
+                        this.password_login = ''
+                        console.log(this.usuario);
+                    })
+                    .catch((error) => {
+                        var errorCode = error.code;
+                        var errorMessage = error.message;
+                        console.log(errorCode)
+                        console.log(errorMessage)
+                    });
+                }
+            },
+            logOut(){
+                firebase.auth().signOut()
+                this.isLogged = false
+                this.page = 'Home'
+                this.usuario = null
+                this.alias = "Anonimo"
+                console.log("Signed out");
+            },
     },
     computed: {
         filtrar(){
             this.filtered = this.results.filter(game => 
-                game.team1.toLowerCase().includes(this.search.toLowerCase()) ||
-                game.team2.toLowerCase().includes(this.search.toLowerCase()) ||
-                game.location.toLowerCase().includes(this.search.toLowerCase()) 
+                game.team1.toLowerCase().trim().includes(this.search.toLowerCase()) ||
+                game.team2.toLowerCase().trim().includes(this.search.toLowerCase()) ||
+                game.location.toLowerCase().trim().includes(this.search.toLowerCase()) 
             )
         },
         changeTitle(){
             document.title = `MDHL | ${this.page}` 
-        }
-
+        },
+    
         },            
 }).mount('#app')
+
+// https://github.com/firebase/quickstart-js/blob/master/database/scripts/main.jshttps://github.com/firebase/quickstart-js/blob/master/database/scripts/main.js
